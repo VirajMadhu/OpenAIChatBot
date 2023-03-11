687125357f641681d5219bdd7393deba72bcf148
Viraj Madhushan <81850588+VirajMadhu@users.noreply.github.com>
2023-03-11 11:57:39 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -3,8 +3,7 @@ import user from "./assets/user.svg";
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
-// const serverApi = "http://localhost:5000/";
-const serverApi = "https://openai-207p.onrender.com/";
+const serverApi = "http://localhost:5000/";
 
 let loadInterval;
 
